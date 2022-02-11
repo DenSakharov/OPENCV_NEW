6503ce75609839805f708aa1f705852f5c97f1a8
Денис Сахаров <sakhirsp@gmail.com>
2022-02-11 19:35:53 +0300

    Добавьте файлы проекта.

---
+++ OPENCV_NEW.cpp
@@ -0,0 +1,32 @@
+﻿#include "opencv2\imgproc.hpp"
+#include "opencv2\objdetect\objdetect.hpp"
+#include "opencv2\videoio\videoio.hpp"
+#include "opencv2\imgcodecs\imgcodecs.hpp"
+#include "opencv2\core\core.hpp"
+#include "opencv2\highgui.hpp"
+#include <vector>
+#include <stdio.h>
+#include <windows.h>
+#include <iostream>
+#include <time.h>
+using namespace cv;
+using namespace std;
+int main(int argc, const char** argv)
+{
+    VideoCapture cap(0);
+    for (;;)
+    {
+        if (!cap.isOpened()) {
+            cout << "Video Capture Fail" << endl;
+            break;
+        }
+        else {
+            Mat img;
+            cap >> img;
+            namedWindow("Video", WINDOW_AUTOSIZE);
+            imshow("Video", img);
+            int key2 = waitKey(20);
+        }
+    }
+    return 0;
+}
